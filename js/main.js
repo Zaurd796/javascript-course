2e015dc0313afa773f1e75e0203fae0233301955
Zaur <zaurdysh@gmail.com>
2022-01-03 19:12:38 +0300

    первое задание

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-let a = false,
-    b = true;
+'use strict';
 
-  console.log(a || b);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
